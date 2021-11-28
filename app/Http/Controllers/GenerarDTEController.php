933e43473ae80b9b9910aae1b47fd7a18b4379b7
MoisesCuevas <moisescuevas@8i.cl>
2021-11-28 15:23:23 -0300

    Primer commit creacion de controlador

---
+++ app/Http/Controllers/GenerarDTEController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class GenerarDTEController extends Controller
+{
+    public function index()
+    {
+        echo "Index Controlador GenerarDTE SII";
+    }
+}
